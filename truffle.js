2d8bc66d41b0aad79d76bd23a4b4bae122403120
KIUCHI Satoshinosuke <scholar@hayabusa-lab.jp>
2018-08-21 11:20:56 +0900

    ropstenへのデプロイ対応

--- truffle.js
+++ truffle.js
@@ -12,15 +12,30 @@
  *   },
  */
 
-require("babel-register");
+require("dotenv").config();
+require("babel-register")({
+  ignore: /node_modules/,
+});
 require("babel-polyfill");
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = process.env.MNEMONIC;
 
 module.exports = {
   networks: {
     development: {
-      host: "ganache",
+      host: "localhost",
       port: 8545,
       network_id: "*",
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/8870b78fa02d47eaa77ccfc9ec84ab01")
+      },
+      network_id: "3",
+      gas: 4500000,
+      gasPrice: 100,
     },
   },
 };
